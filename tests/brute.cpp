666584d695c861704784ce2f22a51658143ce04d
Mohammad Zaid <zaidcoder@gmail.com>
2024-07-03 17:00:07 +0530

    tests added

---
+++ tests/brute.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solveTests()
+{
+    //take input from file input.txt and output to file output.txt
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int N,target;
+    while(cin>>N)
+    {
+        cin>>target;
+        vector<long long> val(N);
+        for(int i = 0; i < N; i++) cin>>val[i];
+        int ans = 0;
+        for(int i = 0; i < (1<<N); i++)
+        {
+            long long sum = 0;
+            for(int j = 0; j < N; j++)
+            {
+                if(i&(1<<j)) sum+=val[j];
+            }
+            ans += (sum==target);
+        }
+        cout<<ans<<endl;
+    }
+    return ;
+}
+int main()
+{
+    solveTests();
+}
