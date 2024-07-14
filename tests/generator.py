e17a94911b5c754f0449698c0a86b047d397bd0c
Mohammad Zaid <zaidcoder@gmail.com>
2024-07-14 19:27:54 +0530

    benchmark function added

--- tests/generator.py
+++ tests/generator.py
@@ -8,18 +8,38 @@ def generate_test_case(n, k, max_value):
 
 def write_test_case_to_file(filename, test_case):
     array, k = test_case
-    # with open(filename, 'w') as f:
-    with open(filename,'a') as f:
+    with open(filename, 'w') as f:
+    # with open(filename,'a') as f:
         f.write(f"{len(array)} {k}\n")
         f.write(' '.join(map(str, array)) + '\n')
         f.write('\n')
 
-for i in range(100):
-    n = random.randint(10, 15)
-    k = random.randint(10, 15)
-    max_value = random.randint(10, 18)
+def generate_small_tests(num_tests):
+    for _ in range(num_tests):
+        n = random.randint(10, 15)
+        k = random.randint(10, 15)
+        max_value = random.randint(10, 18)
 
-    test_case = generate_test_case(n, k, max_value)
+        test_case = generate_test_case(n, k, max_value)
 
-    # write_test_case_to_file('test_case.txt', test_case)
-    write_test_case_to_file('tests/input.txt',test_case)
+        # write_test_case_to_file('test_case.txt', test_case)
+        write_test_case_to_file('tests/input.txt',test_case)
+
+def generate_benchmark_tests():
+
+    min_value = 600
+    max_value = 1000
+
+    for N in [30]:
+        array = [random.randint(min_value, max_value) for _ in range(N)]
+        sum = 0
+        for i in range(N-10):
+            sum += array[i]
+        K = sum 
+        write_test_case_to_file('test_case.txt',(array,K))
+
+
+if __name__ == '__main__':
+    generate_benchmark_tests()
+    # generate_small_tests(100)
+    pass
